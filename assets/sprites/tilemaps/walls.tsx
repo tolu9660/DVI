04ef814a7fc618ed95b16f7ac158f7922b357c37
tolu9660 <57481524+tolu9660@users.noreply.github.com>
2022-02-17 21:33:18 +0100

    nuevos fondos creados
    Se ha creado una nueva plataforma, a la que FALTA METER LA FISICA

--- assets/sprites/tilemaps/walls.tsx
+++ assets/sprites/tilemaps/walls.tsx
@@ -1,4 +1,2 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.1" name="walls" tilewidth="32" tileheight="32" tilecount="56" columns="8">
- <image source="../../../../../../Downloads/wall2.png" width="280" height="224"/>
-</tileset>
+<tileset version="1.8" tiledversion="1.8.1" name="walls" tilewidth="1" tileheight="1" tilecount="0" columns="0"/>
