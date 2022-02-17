3b045abf6152c960a67c6e28d5b1914cd98bd022
Alvaro <alvgom14@ucm.es>
2022-02-17 14:48:41 +0100

    Update piedra.js

--- src/piedra.js
+++ src/piedra.js
@@ -12,7 +12,7 @@ export default class piedra extends Phaser.GameObjects.Sprite{
         super(scene, x, y, 'piedra');
         this.scene.add.existing(this);
         this.scene.physics.add.existing(this, true);
-        this.scene.physics.add.collider(this, player); 
+        this.scene.physics.add.collider(this, player); //comentario
     }
 
 }
